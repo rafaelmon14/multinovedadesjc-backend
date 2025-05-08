7dcad60c834c225e5b121e865cddde4c47da0bfe
Rafael Montenegro <rafaelmon14@gmail.com>
2025-05-08 17:54:09 -0500

    primer commit

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,9 +1,22 @@
-export default [
-  'strapi::logger',
+module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:3000', 
+        '127.0.0.1',
+        'ea8a-190-12-13-23.ngrok-free.app',
+        'https://ea8a-190-12-13-23.ngrok-free.app'
+        
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      
+    },
+  },
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,18 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
+  url: env('PUBLIC_URL', 'http://127.0.0.1:1337'), // opcional
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+  // Agrega esta configuración
+  allowedHosts: [
+    'localhost',
+    '127.0.0.1',
+    'ea8a-190-12-13-23.ngrok-free.app',
+    'https://ea8a-190-12-13-23.ngrok-free.app'
+  ],
 });
