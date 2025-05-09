f46b69f19577863815d0be0d49bea6224a229cf5
Rafael Montenegro <rafaelmon14@gmail.com>
2025-05-09 17:03:25 -0500

    test server

--- config/server.ts
+++ config/server.ts
@@ -4,13 +4,14 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
-  url: env('PUBLIC_URL', 'http://127.0.0.1:1337'), // opcional
+  url: env('RENDER_EXTERNAL_URL', 'http://127.0.0.1:1337'), // opcional
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
   // Agrega esta configuración
   allowedHosts: [
     'https://multinovedadesjc.vercel.app',
+    'https://multinovedadesjc-backend.onrender.com',
     'localhost',
     '127.0.0.1',
     
