83bf064f476e5366fa0a6157f0983f0b1a4778df
Rafael Montenegro <rafaelmon14@gmail.com>
2025-05-09 18:45:50 -0500

    add: cloudinary

--- config/plugins.ts
+++ config/plugins.ts
@@ -4,6 +4,12 @@ module.exports = ({ env }) => ({
     'users-permissions': {
         config: {
             jwtSecret: env('JWT_SECRET'),
-        },
+            provider: '@strapi/cloudinary',
+                providerOptions: {
+                    cloud_name: env('CLOUDINARY_NAME'),
+                    api_key: env('CLOUDINARY_API_KEY'),
+                    api_secret: env('CLOUDINARY_API_SECRET'),
+                },
+            },
     },
  });
